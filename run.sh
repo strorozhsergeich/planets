022cf07cea8df0473aaf3154db7faa01915b8e8c
storozhsergeich <28973204+strorozhsergeich@users.noreply.github.com>
2022-11-16 14:57:58 +1100

    Sart notes on Mars a base

---
+++ run.sh
@@ -0,0 +1,9 @@
+ git config --local user.name 'Ivan Tee'
+ git config --local user.email 'ivan.teliatnikov@gmail.com'
+ git config --local core.autocrlf input
+ git config --local core.edutor 
+ git config --local core.edutor "vim"
+ git config --local init.defaultBranch 
+ git config --local init.defaultBranch "main"
+ git config --local color.status auto
+ git config --local color.ui true
