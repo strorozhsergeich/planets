a2741e9422acb6bdfe06c038b5dc55ed1fda5078
storozhsergeich <28973204+strorozhsergeich@users.noreply.github.com>
2022-11-18 14:22:21 +1100

    Removing references to my private e-mail

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
- git config --local user.name 'Ivan Tee'
- git config --local user.email 'ivan.teliatnikov@gmail.com'
+
+ git config --local user.name 'storozhsergeich'
+ git config --local user.email ${prinvate_email}
  git config --local core.autocrlf input
  git config --local core.edutor 
  git config --local core.edutor "vim"
@@ -7,8 +8,8 @@
  git config --local init.defaultBranch "main"
  git config --local color.status auto
  git config --local color.ui true
- git filter-branch --env-filter 'if [ "$GIT_AUTHOR_EMAIL" = "ivan.teliatnikov@gmail.com" ]; then
-     GIT_AUTHOR_EMAIL=user.email=28973204+strorozhsergeich@users.noreply.github.com;
-     GIT_AUTHOR_NAME="storozhsergeich";
+ git filter-branch --env-filter 'if [ "$GIT_AUTHOR_EMAIL" = "user.email=28973204+strorozhsergeich@users.noreply.github.com" ]; then
+     GIT_AUTHOR_EMAIL='28973204+strorozhsergeich@users.noreply.github.com';
+     GIT_AUTHOR_NAME=storozhsergeich;
      GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL;
      GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; fi' -- --all
