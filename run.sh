8ac0761d35d946495c7f74a15a7e3e98f53e4cee
storozhsergeich <28973204+strorozhsergeich@users.noreply.github.com>
2022-11-18 14:13:15 +1100

    Added snipit to change user.email for all previous commits

--- run.sh
+++ run.sh
@@ -7,3 +7,8 @@
  git config --local init.defaultBranch "main"
  git config --local color.status auto
  git config --local color.ui true
+ git filter-branch --env-filter 'if [ "$GIT_AUTHOR_EMAIL" = "ivan.teliatnikov@gmail.com" ]; then
+     GIT_AUTHOR_EMAIL=user.email=28973204+strorozhsergeich@users.noreply.github.com;
+     GIT_AUTHOR_NAME="storozhsergeich";
+     GIT_COMMITTER_EMAIL=$GIT_AUTHOR_EMAIL;
+     GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"; fi' -- --all
